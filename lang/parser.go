5a9d108644077c0df2d928599bba53d4cfb3ee1f
Jeremy Tregunna <jeremy.tregunna@me.com>
2010-06-22 04:20:42 -0400

    Added more to the Lexer

--- lang/parser.go
+++ lang/parser.go
@@ -18,6 +18,8 @@
 package parser
 
 import (
+	"strings"
+	"unicode"
 	"container/vector"
 	//"./europa"
 )
@@ -39,19 +41,23 @@ const (
 )
 
 type Lexer struct {
-	input string
-	current *Token
-	next *Token
+	input *strings.Reader
+	current *strings.Reader
+	next *strings.Reader
 }
-
 type ILexer interface {
+	Consume()
+	CurrentRune() int
+	NextRune()
+	Lex()
+	ParseArguments() vector.Vector
+	ParseExpression() vector.Vector
 }
 
 type Token struct {
 	name string
 	arguments vector.Vector
 }
-
 type IToken interface {
 	SetName(string)
 	GetName() string
@@ -75,10 +81,91 @@ func (tok *Token) Equal(token IToken) bool {
 	return tok.name == token.GetName()
 }
 
-func NewLexer(str string) *Lexer {
+func NewLexer(str *strings.Reader) *Lexer {
 	r := new(Lexer)
 	r.input = str
-	r.current = nil
-	r.next = nil
+	r.current = strings.NewReader("")
+	r.next = strings.NewReader("")
+	r.Consume()
+	r.Consume()
 	return r
+}
+func (lex *Lexer) Consume() {
+	lex.current = lex.next
+	lex.Lex()
+}
+func (lex *Lexer) CurrentRune() int {
+	r, _, _ := lex.input.ReadRune()
+	return r
+}
+func (lex *Lexer) NextRune() {
+	tmp := (*lex.input)[1:]
+	lex.input = &tmp
+}
+func (lex *Lexer) ParseIdent() {
+	r, size, _ := lex.input.ReadRune()
+	var i int
+	for i = 0; i < len(*lex.input) && unicode.IsLetter(r); i += size {}
+	tmp := (*lex.input)[0:i]
+	lex.next = &tmp
+	tmp = (*lex.input)[i:]
+	lex.input = &tmp
+}
+func (lex *Lexer) ParseNumber() {
+	for i, r := range *lex.input {
+		if !unicode.IsDigit(r) { break }
+		tmp := (*lex.input)[0:i]
+		lex.next = &tmp
+		tmp = (*lex.input)[i:]
+		lex.input = &tmp
+	}
+}
+func (lex *Lexer) ParseArguments() vector.Vector {
+	var (
+		args vector.Vector
+		arg vector.Vector
+	)
+	arg = nil
+	lparenRune := strings.NewReader("(")
+	commaRune := strings.NewReader(",")
+	for lex.current != nil {
+		if lex.current == lparenRune {
+			if arg != nil {
+				args.Push(arg)
+			}
+		} else if lex.current == commaRune {
+			args.Push(arg)
+			arg = nil
+			lex.Consume()
+		} else {
+			arg = lex.ParseExpression()
+		}
+	}
+	
+	return args
+}
+func (lex *Lexer) ParseExpression() vector.Vector {
+	var tree vector.Vector
+}
+func (lex *Lexer) Lex() {
+	newlineRune, _, _ := strings.NewReader("\n").ReadRune()
+	spaceRune, _, _ := strings.NewReader(" ").ReadRune()
+	
+	if lex.input == strings.NewReader("") {
+		lex.next = nil
+	} else if lex.CurrentRune() == newlineRune {
+		lex.next = strings.NewReader(";")
+		lex.NextRune()
+	} else if lex.CurrentRune() == spaceRune {
+		lex.NextRune()
+		lex.Lex()
+	} else if unicode.IsLetter(lex.CurrentRune()) {
+		lex.ParseIdent()
+	} else if unicode.IsDigit(lex.CurrentRune()) {
+		lex.ParseNumber()
+	} else {
+		tmp, _ := lex.input.ReadByte()
+		lex.next = strings.NewReader(string(tmp))
+		lex.NextRune()
+	}
 }
